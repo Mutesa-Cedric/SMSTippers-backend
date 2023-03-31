c3d45033c7bbc698fcdaab88ca8edacd86f2f65b
Mutesa-Cedric <mutesacedric@gmail.com>
2023-03-31 23:49:29 +0200

    prode & dev env

--- src/utils/dbConnection.ts
+++ src/utils/dbConnection.ts
@@ -2,7 +2,7 @@ import mongoose = require("mongoose");
 
 const dbUrl_dev = process.env.MONGOURL_DEV as string;
 const dbUrl_prod = process.env.MONGOURL_PROD as string;
-const dbUrl = process.env.NODE_ENV === "production" ? dbUrl_prod : dbUrl_dev;
+const dbUrl = process.env.NODE_ENV === "development" ? dbUrl_dev : dbUrl_prod;
 
 export const dbConnection = async () => {
     // try {
