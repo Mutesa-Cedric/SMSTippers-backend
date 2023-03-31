76eeea46ea822a7c72ec98c842022cc9ccdb424f
Mutesa-Cedric <mutesacedric@gmail.com>
2023-03-31 22:53:57 +0200

    update(dbConnector) : prod & dev urls

--- src/utils/dbConnection.ts
+++ src/utils/dbConnection.ts
@@ -1,14 +1,12 @@
 import mongoose = require("mongoose");
 
-const dbUrl = process.env.MONGOURL as string;
+const dbUrl_dev = process.env.MONGOURL_DEV as string;
+const dbUrl_prod = process.env.MONGOURL_PROD as string;
+const dbUrl = process.env.NODE_ENV === "production" ? dbUrl_prod : dbUrl_dev;
 
 export const dbConnection = async () => {
     try {
-        await mongoose.connect(dbUrl, {
-            // useNewUrlParser: true,
-            // useUnifiedTopology: true,
-            // useCreateIndex: true,
-        });
+        await mongoose.connect(dbUrl);
         console.log('Database connected');
     } catch (error) {
         console.log(error);
