06327472f0cd13f9cd879463c9e460a5d7131c58
Mutesa-Cedric <mutesacedric@gmail.com>
2023-04-01 15:46:10 +0200

    fix : cors

--- src/server.ts
+++ src/server.ts
@@ -27,6 +27,18 @@ dbConnection().then(() => {
 app.use(bodyParser.json())
 app.use(cookieParser());
 
+// cors
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+
 // router middlewares
 app.use("/auth", authRouter);
 app.use("/tickets", isAuthenticated, ticketsRouter);
