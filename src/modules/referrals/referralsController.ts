bdab8826f0de4af3c4f3733cf4f8fb5dc24ac110
Mutesa-Cedric <mutesacedric@gmail.com>
2023-05-05 16:13:41 +0200

    update : referral earnings

--- src/modules/referrals/referralsController.ts
+++ src/modules/referrals/referralsController.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import User from "../user/userModel";
+import { verifyToken } from "../../utils/jwt";
+import EarnedFromReferrals from "../referrals/earnedFromReferralsModel";
 
 export const createReferral = async (req: Request, res: Response) => {
+    console.log("create referral was hit");
     try {
         const { promoCode, userId } = req.body;
 
-        if (!userId || !promoCode) return res.status(400).json({ message: "No user id provided!" });
+        if (!userId || !promoCode || typeof userId === undefined) return res.status(400).json({ message: "No user id provided!" });
         if (!promoCode) {
             return res.status(400).json({ message: "No promo code provided!" });
         }
@@ -35,12 +38,85 @@ export const getReferrals = async (req: Request, res: Response) => {
         const { userId } = req.query;
         if (!userId) return res.status(400).json({ message: "No user id provided!" });
 
-        const user = await User.findById(userId).populate("referrals");
+        const user = await User.findById(userId).populate("referrals").sort({ createdAt: -1 });
+
 
         if (!user) return res.status(400).json({ message: "Invalid user id!" });
 
-        res.status(200).json({ referrals: user.referrals });
+        // remove password,apiKey,promoCode from referrals
+        user.referrals.forEach((referral: any) => {
+            referral.password = undefined;
+            referral.apiKey = undefined;
+            referral.promoCode = undefined;
+            referral.referredBy = undefined;
+            referral.createdAt = undefined;
+            referral.updatedAt = undefined;
+            referral.referrals = undefined;
+        });
 
+        /*
+            registered today count,registered last week count,registered last month count,registered total count
+        */
+        const registeredToday = user.referrals.filter((referral: any) => {
+            return referral.createdAt >= Date.now() - 24 * 60 * 60 * 1000;
+        });
+        const registeredLastWeek = user.referrals.filter((referral: any) => {
+            return referral.createdAt >= Date.now() - 7 * 24 * 60 * 60 * 1000;
+        });
+        const registeredLastMonth = user.referrals.filter((referral: any) => {
+            return referral.createdAt >= Date.now() - 30 * 24 * 60 * 60 * 1000;
+        });
+
+
+        res.status(200).json({
+            message: "success",
+            registeredTodayCount: registeredToday.length,
+            registeredLastWeekCount: registeredLastWeek.length,
+            registeredLastMonthCount: registeredLastMonth.length,
+            total: user.referrals.length,
+            referrals: user.referrals
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+}
+
+
+export const getEarningsFromReferrals = async (req: Request, res: Response) => {
+    try {
+        const token = req.cookies.token;
+        if (!token) return res.status(403).json({ message: "Anauthorized!" });
+        const { id } = verifyToken(token);
+        if (!id) return res.status(403).json({ message: "Anauthorized!" });
+        const user = await User.findById(id);
+        if (!user) return res.status(403).json({ message: "Anauthorized!" });
+
+        const earnings = await EarnedFromReferrals.find({ earnedBy: id });
+        /*
+            group by last day, last week, last month, and total
+        */
+        const lastDayEarnings = earnings.filter((earning: any) => {
+            return earning.createdAt >= Date.now() - 24 * 60 * 60 * 1000;
+        });
+        const lastWeekEarnings = earnings.filter((earning: any) => {
+            return earning.createdAt >= Date.now() - 7 * 24 * 60 * 60 * 1000;
+        }
+        );
+        const lastMonthEarnings = earnings.filter((earning: any) => {
+            return earning.createdAt >= Date.now() - 30 * 24 * 60 * 60 * 1000;
+        }
+        );
+        const totalEarnings = earnings;
+        res.status(200).json({
+            message: "success",
+            earnings: {
+                lastDay: lastDayEarnings.map((earning: any) => earning.amount).reduce((a: any, b: any) => a + b, 0),
+                lastWeek: lastWeekEarnings.map((earning: any) => earning.amount).reduce((a: any, b: any) => a + b, 0),
+                lastMonth: lastMonthEarnings.map((earning: any) => earning.amount).reduce((a: any, b: any) => a + b, 0),
+                total: totalEarnings.map((earning: any) => earning.amount).reduce((a: any, b: any) => a + b, 0)
+            }
+        });
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: "Internal server error" });
--- src/modules/referrals/referralsRouter.ts
+++ src/modules/referrals/referralsRouter.ts
@@ -1,10 +1,11 @@
 import express = require("express");
-import { createReferral, getReferrals } from "./referralsController";
+import { createReferral, getEarningsFromReferrals, getReferrals } from "./referralsController";
 
 const router = express.Router();
 
 router.post("/create", createReferral);
 router.get("/get", getReferrals);
+router.get("/get-earnings", getEarningsFromReferrals);
 
 
 export default router;
