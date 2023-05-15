c3d8ad322937ff6868c6e938acc0deda317536d4
Mutesa-Cedric <mutesacedric@gmail.com>
2023-05-15 11:38:42 +0200

    feat : dashboard stats

--- src/modules/user/statsController.ts
+++ src/modules/user/statsController.ts
@@ -3,60 +3,74 @@ import { verifyToken } from "../../utils/jwt";
 import Order from "../orders/orderModel";
 import Rental from "../rentals/rentalModel";
 import Deposit from "../deposit/depositModel";
+import {
+    getTotalStats,
+    getDailyStats,
+    getWeeklyStats,
+    getMonthlyStats,
+    getChartStats
+} from "./statsService"
 
 
 export const getStats = async (req: Request, res: Response) => {
     try {
-        const token = req.cookies.token;
-        if (!token) {
-            return res.status(401).json({ message: "Unauthorized" });
-        }
-        const { id } = await verifyToken(token);
+        const { user_id } = req.query;
 
-        /*
-        structure : 
-        ----------
-        {
-            totalDeposits:number,
-            totalSpent:number,//orders and rentals made(their prices added together),
-            totalVerifications:number,
-            daily:{
-                deposits:number,
-                spent:number,
-                verifications:number,
-            },
-            weekly:{
-                deposits:number,
-                spent:number,
-                verifications:number,
-            },
-            monthly:{
-                deposits:number,
-                spent:number,
-                verifications:number,
-            },
+        let userId: string;
+        if (user_id) {
+            userId = user_id as string;
+        } else {
 
-            // for charts, I need to provide data for all those fields for each month for the last year.
+            const token = req.cookies.token;
 
-            chartData:{
-                1:{//thi is for january
-                    deposits:number,
-                    spent:number,
-                    verifications:number,
-                }
-                // and so on for the rest of the months
+            if (!token) {
+                return res.status(401).json({ message: "Unauthorized" });
             }
+            const { id } = await verifyToken(token);
+            userId = id;
         }
 
-        */
-        const orders = await Order.find({ user: id });
-        const rentals = await Rental.find({ user: id });
-        const deposits = await Deposit.find({ user: id });
 
-        const totalSpent = orders.reduce((acc, order) => acc + order.price, 0) + rentals.reduce((acc, rental) => acc + rental.price, 0);
+        const orders = await Order.find({ user: userId });
+        const rentals = await Rental.find({ user: userId });
+        const deposits = await Deposit.find({ user: userId });
 
+        const totalStats = getTotalStats({ orders, rentals, deposits });
+        const dailyStats = getDailyStats({ orders, rentals, deposits });
+        const weeklyStats = getWeeklyStats({ orders, rentals, deposits });
+
+        const monthlyStats = getMonthlyStats({ orders, rentals, deposits });
+        const chartStats = getChartStats({ orders, rentals, deposits });
+
+        res.status(200).json({
+            message: "success",
+            stats: {
+                totalDeposits: totalStats.deposits,
+                totalSpent: totalStats.spent,
+                totalVerifications: totalStats.verifications,
+                daily: {
+                    deposits: dailyStats.deposits,
+                    spent: dailyStats.spent,
+                    verifications: dailyStats.verifications,
+                },
+                weekly: {
+                    deposits: weeklyStats.deposits,
+                    spent: weeklyStats.spent,
+                    verifications: weeklyStats.verifications,
+                },
+                monthly: {
+                    deposits: monthlyStats.deposits,
+                    spent: monthlyStats.spent,
+                    verifications: monthlyStats.verifications,
+                }
+            },
+            chartData: chartStats.chartData
+        });
 
     } catch (error) {
-
+        console.log(error);
+        res.status(500).json({
+            message: "Internal server error"
+        })
     }
 }
--- src/modules/user/userRouter.ts
+++ src/modules/user/userRouter.ts
@@ -6,6 +6,7 @@ import {
     updateProfile,
     updatePromoCode
 } from "./userController";
+import { getStats } from "./statsController"
 import { findUserByPromoCode } from "./userService";
 
 const router = express.Router();
@@ -15,5 +16,6 @@ router.put("/update-password", isAuthenticated, updatePassword);
 router.get("/update-api-key", isAuthenticated, updateApiKey);
 router.get("/find-by-promocode", isAuthenticated, findUserByPromoCode);
 router.put("/update-promo-code", isAuthenticated, updatePromoCode);
+router.get("/get-stats", getStats);
 
 export default router;
