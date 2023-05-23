9b440385050413f05ec76a059648545177106dad
Mutesa-Cedric <mutesacedric@gmail.com>
2023-05-23 16:43:49 +0200

    tryna fix cors

--- src/server.ts
+++ src/server.ts
@@ -31,13 +31,13 @@ app.use(cookieParser());
 // @ts-ignore   
 // app.use(rawBody)
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", req.headers.origin ? req.headers.origin : "http://localhost:3000");
-//     res.setHeader("Access-Control-Allow-Credentials", "true");
-//     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
-//     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
-//     next();
-// });
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", req.headers.origin ? req.headers.origin : "https://smstippers.com");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    next();
+});
 
 app.use((req, res, next) => {
     console.log(req.originalUrl, "\t", req.method, "\t", req.url);
